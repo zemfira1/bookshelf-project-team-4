d2515bb58cc6e02db5e4abb12c0533d20b6a239a
zemfira1 <craftyzema@gmail.com>
2023-06-30 02:53:05 -0400

    add parts

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './js/toggle-switch';
+import './js/modal-menu';
+import './js/all-categories';
+import './js/support';
+import './js/all-books';
+import './js/pop-up';
+import './js/shoplist';
