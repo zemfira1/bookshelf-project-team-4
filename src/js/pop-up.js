23e59588ecc60a0fa5d41a4a5ca9b132a945ac0c
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-01 21:47:21 +0300

    Update pop-up.js

---
+++ src/js/pop-up.js
@@ -0,0 +1,15 @@
+const bookImage = document.querySelector(".book-image");
+const bookTitle = document.querySelector(".book-title");
+const bookAuthor = document.querySelector(".book-author");
+const bookDescription = document.querySelector(".book-description");
+const addToListButton = document.querySelector("add-to-list-button");
+
+/*
+
+https://books-backend.p.goit.global/books/bookId
+
+"js-books"
+
+#icon-x-close
+
+*/
